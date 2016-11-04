d2f7efc5b8d1e575a309df66feeeb8178c99f723
quinnschwartz <quinnschwartz@gmail.com>
2016-11-04 10:28:47 -0700

    write javascript front and back end code to fulfill spec 1 of spec list.

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -1,7 +1,20 @@
 //Business Logic
+function Pizza(size) {
+  this.newSize = size;
+}
 
+Pizza.prototype.calculatePrice = function(newPizza) {
+  var price = 0;
 
-
+  if (this.newSize === "Small (7 Dollars)") {
+    price += 7;
+  } else if (this.newSize ===  "Medium (12 Dollars)") {
+    price += 12;
+  } else {
+    price += 15;
+  }
+  return price;
+}
 
 
 
@@ -13,6 +26,17 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+
+    var inputSize = $("select#size").val();
+    // $("input:checkbox[name=toppings]:checked").each(function(){
+    //   var inputToppings = $(this).val();
+    // });
+    var newPizza = new Pizza(inputSize);
+    newPizza.price = newPizza.calculatePrice(newPizza);
+
+    $("div.show-confirmation").show();
+    $("span.size").text(inputSize);
+    $("span.price").text(newPizza.price);
 
   });
 });
