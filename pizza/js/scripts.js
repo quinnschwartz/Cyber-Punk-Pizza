483d3a6ba097d8426d2826b2d2d99dfcf1f28bb3
quinnschwartz <quinnschwartz@gmail.com>
2016-11-04 16:42:31 -0700

    add a prototype to the business logic that returns the string cheese if there's no toppings selected and returns the array of toppings if there are toppings selected.

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -5,28 +5,34 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.calculatePrice = function(newPizza) {
-  var price = this.newToppings;
+  var price = this.newToppings.length;
   var small = 7;
   var medium = 12;
   var large = 16;
   if (this.newSize === "Small") {
     price += small;
   } else if (this.newSize ===  "Medium") {
-    price = this.newToppings * 1.5;
+    price = this.newToppings.length * 1.5;
     price += medium;
   } else {
-    price = this.newToppings * 2;
+    price = this.newToppings.length * 2;
     price += large;
   }
   return price;
 }
 
+Pizza.prototype.countToppings = function(newPizza) {
+  if (this.newSize === "Small" || this.newSize === "Medium" || this.newSize === "Large" && this.newToppings === 0) {
+    return "Cheese";
+  } else {
+    return this.newToppings;
+  }
+}
 
 //User Interface Logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-
     var inputSize = $("select#size").val();
     var toppingsArray = [];
     $("input:checkbox[name=toppings]:checked").each(function(){
@@ -34,16 +40,14 @@ $(document).ready(function(){
       toppingsArray.push(inputToppings);
     });
 
-    var toppingsTotal = toppingsArray.length;
-
-    var newPizza = new Pizza(inputSize, toppingsTotal);
+    var newPizza = new Pizza(inputSize, toppingsArray);
 
     newPizza.price = newPizza.calculatePrice(newPizza);
+    newPizza.toppings = newPizza.countToppings(newPizza);
 
     $("div.show-confirmation").show();
     $("span.size").text(inputSize);
-    $("span.toppingsArray").text(toppingsArray);
+    $("span.toppings").text(newPizza.toppings);
     $("span.price").text(newPizza.price);
-
   });
 });
